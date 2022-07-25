b87fad47243950fd1cb59310d7b1ebd51b029b99
Cristian García <cristianalejandrogarciadiaz123@gmail.com>
2022-07-25 17:03:42 -0600

    conchetumadre

---
+++ src/main/java/org/cristiangarcia/av/Av.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.cristiangarcia.av;
+import org.cristiangarcia.login.Login;
+/**
+ *
+ * @author crist
+ */
+public class Av {
+    public static void main(String[] args) {
+        Login rt = new Login();
+        rt.setVisible(true);
+    }
+}
